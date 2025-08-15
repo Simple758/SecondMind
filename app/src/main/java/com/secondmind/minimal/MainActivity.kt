0602252a4a6077e037180bc335eed1a191abe2ea
Raunak Sharma <rounaksharma333@gmail.com>
2025-08-15 15:50:10 +0200

    stabilize: compose + material components theme, locked repos/plugins

--- app/src/main/java/com/secondmind/minimal/MainActivity.kt
+++ app/src/main/java/com/secondmind/minimal/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      MaterialTheme { Surface(modifier = Modifier.fillMaxSize()) { HomeScreen() } }
+      MaterialTheme { Surface(Modifier.fillMaxSize()) { HomeScreen() } }
     }
   }
 }
